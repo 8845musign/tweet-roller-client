b97a56c8c8193ef6c89d01945e60c1730d381f3a
8845musign <8845musign@gmail.com>
2017-10-01 11:58:32 +0900

    fix lint error

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,37 @@
-"use strict";
-
-var webpack = require("webpack");
-var path = require("path");
-
 const configBase = {
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ['*', '.js', '.jsx']
   },
   module: {
     loaders: [
       {
         exclude: /node_modules/,
         test: /\.jsx?$/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       }
     ]
-  },
+  }
 }
 
 const mainConfig = Object.assign({}, configBase, {
-  entry: "./src/app.js",
-  target: "electron-main",
+  entry: './src/app.js',
+  target: 'electron-main',
   node: {
     __dirname: false,
     __filename: false
   },
   output: {
     path: `${__dirname}/dist`,
-    filename: "index.js"
+    filename: 'index.js'
   }
-});
+})
 
 const rendererConfig = Object.assign({}, configBase, {
   entry: './src/renderer/app.js',
-  target: "electron-renderer",
+  target: 'electron-renderer',
   output: {
-    filename: "renderer/bundle.js"
+    filename: 'renderer/bundle.js'
   }
-});
+})
 
-
-module.exports = [mainConfig, rendererConfig];
+module.exports = [mainConfig, rendererConfig]
